83529532d7a2b0c9c43e73ba590ede21626ef12e
VinceV0 <104742367+VinceV0@users.noreply.github.com>
2025-09-17 11:08:16 +1200

    Generated item code now uniquely generated

--- Listing.py
+++ Listing.py
@@ -3,19 +3,36 @@
 # This code is specifically derived from the UML Class Diagram
 # This defines the 'Listing' class
 
+# Any print statements are for testing purposes
 import random
 
 class Listing:
+    # Array of known UNIQUE codes
+    uniqueCodes = []
     # Parameter datatypes are recommended but not enforced
     def __init__(self,  listingTitle:str, listingPrice:float, condition:str):
-        # Generate a random 6-digit integer
-        self.gen = random.randint(100000, 999999)
         # Stores Object ID
-        self.__code = self.gen
+        self.__code = self.genUnique()
         self._listingTitle = listingTitle
         self._listingPrice = listingPrice
         self._condition = condition
     
+    # Generate a UNIQUE 6-digit integer
+    def genUnique(self):
+        # Generate a random 6-digit integer
+        self.gen = random.randint(100000, 999999)
+        print(self.gen)
+        if self.gen in self.uniqueCodes:
+            # If found to be a duplicate number, call itself again to generate another number
+            print("Duplicate value")
+            self.genUnique()
+        else:
+            # If the generated number is unique it will append to the list of known unique numbers
+            self.uniqueCodes.append(self.gen)
+            print("Appended new unique number")
+            # Returns the UNIQUE
+            return self.gen
+
     # If in case the object is directly called it is easily identified by the code and title
     def __str__(self):
         return f"{self.__code}:{self.GetListingTitle()}"
