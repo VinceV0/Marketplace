95a77c54c2c4736b6c9e7a07905ed1eb8e9450d1
VinceV0 <104742367+VinceV0@users.noreply.github.com>
2025-09-19 17:57:08 +1200

    Listing Class Completed - Test Harness

--- Listing.py
+++ Listing.py
@@ -7,12 +7,14 @@
 import random
 
 class Listing:
-    # Array of known UNIQUE codes
-    uniqueCodes = []
+    # Array of known UNIQUE 6 digits numbers
+    uniqueNums = []
     # Parameter datatypes are recommended but not enforced
     def __init__(self,  listingTitle:str, listingPrice:float, condition:str):
+        # Calls a method to generate a unique randomly generated 6-digit number
+        self.gen = self.genUnique()
         # Stores Object ID
-        self.__code = self.genUnique()
+        self.__code = len(self.uniqueNums)
         self._listingTitle = listingTitle
         self._listingPrice = listingPrice
         self._condition = condition
@@ -20,22 +22,22 @@ class Listing:
     # Generate a UNIQUE 6-digit integer
     def genUnique(self):
         # Generate a random 6-digit integer
-        self.gen = random.randint(100000, 999999)
-        print(self.gen)
-        if self.gen in self.uniqueCodes:
+        self.generate = random.randint(100000, 999999)
+        # print(self.generate)
+        if self.generate in self.uniqueNums:
             # If found to be a duplicate number, call itself again to generate another number
-            print("Duplicate value")
+            # print("Duplicate value")
             self.genUnique()
         else:
             # If the generated number is unique it will append to the list of known unique numbers
-            self.uniqueCodes.append(self.gen)
-            print("Appended new unique number")
+            self.uniqueNums.append(self.generate)
+            # print("Appended new unique number")
             # Returns the UNIQUE
-            return self.gen
+            return self.generate
 
     # If in case the object is directly called it is easily identified by the code and title
     def __str__(self):
-        return f"{self.__code}:{self.GetListingTitle()}"
+        return f"{self.__code}:{self.gen}:{self.GetListingTitle()}"
     
     # Getter for Listing Title
     def GetListingTitle(self):
@@ -68,20 +70,20 @@ class Listing:
 
     # Displays the information nicely
     def DisplayListing(self):
-        self._fullinfo = f"{self.GetListingTitle()},{self.GetListingPrice()},{self.GetCondition()}"
+        self._fullinfo = f"{self.GetListingTitle()},${self.GetListingPrice()},{self.GetCondition()}"
         return self._fullinfo
 
 def main():
     # Test harness, putting in 2 objects, changing the first object
 
     # First Object
-    listing1 = Listing("Apple", "2", "New")
+    listing1 = Listing("Apple", "2.00", "New")
     # Printing the object code and the corresponding listing title
     print(listing1)
     # Print using method
     print(f"{listing1.DisplayListing()}\n")
     listing1.ListingTitle = "Pear"
-    listing1.ListingPrice = "3"
+    listing1.ListingPrice = "3.99"
     listing1.Condition = "Used"
     # Reprinting the object code and the corresponding listing title - Should print same code but now different Listing Title
     print(listing1)
@@ -89,11 +91,11 @@ def main():
     print(f"{listing1.DisplayListing()}\n")
     
     # Second object(listing)
-    listing2 = Listing("Banana", "2", "Used")
+    listing2 = Listing("Banana", "2.50", "Used")
     # Printing the object code and the corresponding listing title
     print(listing2)
     # Print using properties
-    print(f"{listing2.ListingTitle},{listing2.ListingPrice},{listing2.Condition}")
+    print(f"{listing2.ListingTitle},${listing2.ListingPrice},{listing2.Condition}")
 
 if __name__ == "__main__":
     main()
