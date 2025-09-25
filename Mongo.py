bf4bf15115f37b6814054d2c3431bde631be476b
VinceV0 <104742367+VinceV0@users.noreply.github.com>
2025-09-25 22:27:35 +1200

    Mongo Integration - Initial Commit

---
+++ Mongo.py
@@ -0,0 +1,35 @@
+# Vince Vagay - 30036567
+# Task 2
+# MongoDB Incorporation
+
+from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
+import json
+
+from MarketPlace import MarketPlace
+from pprint import pprint
+
+uri = 
+
+# Create a new client and connect to the server
+client = MongoClient(uri, server_api=ServerApi('1'))
+
+storedListings = []
+
+try:
+    client.admin.command('ping')
+    # print("Pinged your deployment. You successfully connected to MongoDB!")
+    mydb = client["Marketplace"]            # Pointing to the 'Marketplace' database
+    mycollection = mydb["market"]           # Pointing to the 'market' collection
+
+    listings = mycollection.find({})
+    
+    for listing in listings:
+        print(f"Listing title: '{listing['ListingTitle']}'")
+        listing = MarketPlace(f"{listing['ListingTitle']}", 244.99, "New", "This is a nice watch", "0215644434", "jeff34543@gmail.com")
+        storedListings.append(listing)
+        
+    for x in storedListings:
+        print(x.MarketListingInfo())
+except Exception as e:
+    print(e)
